686f60583918deda1324b1891b44fd6131eda152
Hà Hiệp Thanh <hahiepthanhhhtt@gmail.com>
2026-05-11 22:43:58 +0700

    refactor: replace Abstract Factory interfaces with Singleton pattern implementation

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonPattern;
+
+internal class Singleton
+{
+    private static Singleton instance = null;
+    private Singleton() { }
+
+    public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
